1b876d150f43e7946578fea6a4c194912f3a8f84
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-10-27 16:45:35 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.15 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(left: Int, right: Int): Int {
+        var answer: Int = 0
+        for( i in left..right){
+        var count=0
+        for(j in 1 .. i){
+            if(i%j==0){
+                count++
+            }
+        }
+        if(count%2==0){
+            answer += i
+        }
+        if(count%2==1){
+            answer -= i
+        }
+    }
+        return answer
+    }
+}
