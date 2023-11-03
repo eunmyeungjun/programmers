6289585b0f65cdec79901b5f6872f82cf6103f60
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-11-03 15:53:40 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun solution(t: String, p: String): Int {
     var answer: Int = 0
-
-    for (i in 0 until t.length - p.length + 1) {
-        val sub = t.substring(i, i + p.length)
-        if (sub.toLong() <= p.toLong()) {
-            answer++
+    var count =0    
+        for (i in 0 until t.length - p.length + 1) {
+        val cut = t.substring(i, i + p.length)
+        if (cut.toLong() <= p.toLong()) {
+            count++
         }
     }
-    
+    answer = count
     return answer
 }
 }
