445ae44e8b922bbd15c18b4568c0969fe3718b0c
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-11-16 16:10:04 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun solution(nums: IntArray): Int {
+        var answer = 0
+        val list = mutableListOf<Int>()
+
+        for (i in 0 until nums.size-2){
+            for (j in i+1 until nums.size-1){
+                for (k in j+1 until nums.size){
+                    val hap = nums[i]+nums[j]+nums[k]
+                    var prime = true
+                    for(l in 2 until hap){
+                        if(hap % l == 0){
+                            prime = false
+                            break
+                        }
+                    }
+                    if(prime && hap>1){
+                        list += hap
+                    }
+                }
+            }
+        }
+        answer = list.size
+        return answer
+    }
+}
