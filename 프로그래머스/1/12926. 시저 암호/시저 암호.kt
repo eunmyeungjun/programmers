01ccb477094121018802c7a55007c6eff2704301
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-11-22 11:23:15 +0900

    [level 1] Title: 시저 암호, Time: 14.90 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(s: String, n: Int): String {
+        val S = s.map {
+            when {
+                it in 'a'..'z' -> ((it.toInt() - 'a'.toInt() + n) % 26 + 'a'.toInt()).toChar()
+                it in 'A'..'Z' -> ((it.toInt() - 'A'.toInt() + n) % 26 + 'A'.toInt()).toChar()
+                else -> it
+            }
+        }
+        return S.joinToString("")
+    }
+}
