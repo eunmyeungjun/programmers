2cd095a47159f7542878f032339060b28f0cbb9b
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-11-23 12:37:08 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 7.51 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun solution(survey: Array<String>, choices: IntArray): String {
+        var answer: String = ""
+         var score = mutableMapOf<String,Int>(
+         "R" to 0, "T" to 0 , "C" to 0 , "F" to 0 ,
+         "J" to 0, "M" to 0 , "A" to 0 , "N" to 0
+     )
+
+    for (i in 0 until choices.size){
+        when(choices[i]){
+            1 -> score["${survey[i][0]}"] = (score["${survey[i][0]}"]!! + 3)
+            2 -> score["${survey[i][0]}"] = (score["${survey[i][0]}"]!! + 2)
+            3 -> score["${survey[i][0]}"] = (score["${survey[i][0]}"]!! + 1)
+            4 -> score["${survey[i][0]}"] = (score["${survey[i][0]}"]!!+  0)
+            5 -> score["${survey[i][1]}"] = (score["${survey[i][1]}"]!! + 1)
+            6 -> score["${survey[i][1]}"] = (score["${survey[i][1]}"]!! + 2)
+            7 -> score["${survey[i][1]}"] = (score["${survey[i][1]}"]!! + 3)
+        }
+    }
+    if (score["R"]!! >= score["T"]!!) answer += "R"
+        else  answer += "T"
+       if ( score["C"]!! >= score["F"]!!) answer += "C"
+        else answer += "F"
+       if ( score["J"]!! >= score["M"]!!) answer += "J"
+      else   answer += "M"
+       if ( score["A"]!! >= score["N"]!!)  answer += "A"
+       else  answer += "N"
+        return answer
+    }
+}
