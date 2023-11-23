7e4e03077a12ff383a1660eb223951dcdc2392fa
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-11-23 16:34:56 +0900

    [level 1] Title: 바탕화면 정리, Time: 9.67 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun solution(wallpaper: Array<String>): IntArray {
+        var answer = intArrayOf()
+    var maxX = 0
+    var minX = 100
+    var maxY = 0
+    var minY = 100
+
+    for (i in 0 until wallpaper.size){
+        for (j in 0 until wallpaper[i].length){
+            if (wallpaper[i][j] =='#'){
+               if (i >= maxX){
+                   maxX = i
+               }
+                if (i <= minX){
+                    minX= i
+               }
+                if (j >= maxY){
+                    maxY = j
+                }
+                if (j <= minY){
+                    minY = j
+                }
+
+            }
+
+        }
+
+    }
+    
+    answer += minX
+    answer += minY
+    answer += maxX+1
+    answer += maxY+1
+        return answer
+    }
+}
