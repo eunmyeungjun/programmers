38e4579020fce1d5ca10f3a99ea463364ff5f9ba
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-11-29 09:47:23 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 15.34 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(s: String): String {
+        var answer = ""
+         val S = s.split(" ").map { it.toLowerCase() }.toMutableList()
+    val jaden = S.map { it.replaceFirstChar { it.toUpperCase() } }
+
+    answer += jaden.joinToString(" ")
+        return answer
+    }
+}
