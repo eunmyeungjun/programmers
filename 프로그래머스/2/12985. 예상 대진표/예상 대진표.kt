0900947dffd560c279a12c0674d3bfadf59e0a93
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-12-01 09:24:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun solution(n: Int, a: Int, b: Int): Int {
+        var answer = 0
+var winnerA = a
+    var winnerB = b
+
+    while (true){
+        if ( winnerA == winnerB ){
+            break
+        }
+        if (winnerA % 2 == 0){
+            winnerA /= 2
+        }else {
+            winnerA = (winnerA/2) + 1
+        }
+        if (winnerB % 2 == 0){
+            winnerB /= 2
+        } else {
+            winnerB = (winnerB/2) + 1
+        }
+        answer++
+    }
+        return answer
+    }
+}
