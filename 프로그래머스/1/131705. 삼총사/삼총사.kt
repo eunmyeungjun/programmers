1e8decc4b5c4999c36250a53f71a953b922ff66e
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2023-12-27 09:49:18 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.kt
+++ 프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -5,7 +5,7 @@ class Solution {
         for(i in 0 until number.size-2){
             for(j in i+1 until number.size-1){
                 for(k in j+1 until number.size ){
-                    if(number[i]+number[j]+number[k]==0){
+                    if (number[i] + number[j] + number[k] == 0){
                         count++
                     }
                 }
