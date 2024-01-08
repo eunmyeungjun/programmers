b25169846dcde4d91b7ae9c92e9675077ad2fd34
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-01-08 20:57:05 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.01 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -1,23 +1,18 @@
 class Solution {
     fun solution(food: IntArray): String {
         var answer: String = ""
-         var list= mutableListOf<Int>()
+        var list= mutableListOf<Int>()
 
-    for(i in 1 until food.size){
-        val j =food[i]/2
-        repeat(j){
-            list.add(i)
+        for(i in 1 until food.size){
+            val j =food[i]/2
+            repeat(j){
+                list.add(i)
+            }
         }
-    }
-    val rev=list.reversed()
-    list.add(0)
+        val rev = list.reversed()
+        list.add(0)
 
-    for(k in 0 until list.size){
-        answer +=list[k]
+        answer = (list + rev).joinToString("")
+        return answer
     }
-      for(l in 0 until rev.size){
-          answer +=rev[l]
-      }
-      return answer
-}
 }
