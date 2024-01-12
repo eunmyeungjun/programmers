a88c73f79d216ccb53c08188f5d2c7bb5215604f
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-01-12 21:17:30 +0900

    [level Lv.1] Title: 체육복, Time: 20.57 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/42862. 체육복/체육복.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = n-lost.size
+        var lostStudent =lost.toMutableList().sorted()
+        var remain = reserve.toMutableList().sorted()
+        for(i in lostStudent){
+        if (i in remain){
+            remain = (remain - i ).sorted().toMutableList()
+            lostStudent = (lostStudent-i).sorted().toMutableList()
+            answer ++
+            }
+        } 
+        for(j in lostStudent){
+            
+        if (answer == n) {
+            return answer
+        }
+        
+        if (j-1 in remain) {
+            remain = (remain -(j-1)).sorted().toMutableList()
+            lostStudent =(lostStudent -(j-1)).sorted().toMutableList()
+            answer++
+        }else if(j+1 in remain){
+            remain = (remain - (j+1)).sorted().toMutableList()
+            lostStudent = (lostStudent - (j+1)).sorted().toMutableList()
+            answer++
+            }
+        }
+        return answer
+    }
+}
