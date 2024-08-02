11acaf2f53eb64e57e122519c7db1a7bf85be7d2
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-08-02 19:54:34 +0900

    [level 1] Title: 명예의 전당 (1), Time: 30.25 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
@@ -1,25 +1,22 @@
 class Solution {
     fun solution(k: Int, score: IntArray): IntArray {
         var answer: IntArray = intArrayOf()
-        var honor =mutableListOf<Int>()
-        
+        var honor = mutableListOf<Int>()
         for(i in 0 until score.size){
             if(i<k){
                 honor.add(score[i])
                 answer= answer.plus(honor.minOf{it})
             }
             else{
-                
                 if(score[i]>honor.minOf{it}){
                     honor.add(score[i])
                     honor.remove(honor.minOf{it})
                    answer = answer.plus(honor.minOf{it})
-                }else{
+                } else {
                     answer =answer.plus(honor.minOf{it})
                 }
             }
         }
-        
         return answer
     }
 }
