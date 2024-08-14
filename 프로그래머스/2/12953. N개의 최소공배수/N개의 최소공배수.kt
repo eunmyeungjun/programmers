2f49fd213c7b417e5a36c04bc8d88767f969ef24
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-08-14 23:33:00 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
@@ -1,10 +1,13 @@
 class Solution {
     fun solution(arr: IntArray): Int {
+        
         for (i in 0 until arr.size - 1) {
             arr[i + 1] = (arr[i] * arr[i + 1]) / gongYack(arr[i], arr[i + 1])
         }
+        
        return arr[arr.size - 1]
     }
+    
      fun gongYack(a: Int, b: Int): Int {
         var remain = 0
         var A = a
