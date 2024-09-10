de4349ca2586aa078a1161f11bef0cc8bc7ae2a5
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-09-10 23:08:14 +0900

    [level 1] Title: 덧칠하기, Time: 2.10 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -1,7 +1,6 @@
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
         var answer: Int = 1
-    
 
     var paintStart =section[0] - 0.1
     var painted = paintStart + m
@@ -10,7 +9,6 @@ class Solution {
             paintStart = i -0.1
             painted = paintStart + m
             answer++
-            
         }
     }
         return answer
