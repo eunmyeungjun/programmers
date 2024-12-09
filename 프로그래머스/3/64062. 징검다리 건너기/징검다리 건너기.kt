4ee447bf4bdfc517c7a31f01deeecdbdca577a50
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-12-09 23:35:23 +0900

    [level 3] Title: 징검다리 건너기, Time: 115.21 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.kt
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.kt
@@ -1,31 +1,18 @@
-
 import java.util.Deque
 import java.util.LinkedList
 
-
-
 class Solution {
     fun solution(stones: IntArray, k: Int): Int {
-
-        
-    var answer = Int.MAX_VALUE
+            var answer = Int.MAX_VALUE
     val window : Deque<Int> = LinkedList()
 
     for (i in stones.indices) {
-
-        if (!window.isEmpty() && window.first() == i - k) {
-            window.removeFirst()
-        }
-
-        while (!window.isEmpty() && stones[window.last()] <= stones[i]) {
-            window.removeLast()
-        }
+        if (window.isNotEmpty() && window.first() == i - k) window.removeFirst()
         
+        while (window.isNotEmpty() && stones[window.last()] <= stones[i]) window.removeLast()
         window.addLast(i)
-        
-        if (i >= k - 1) {
-            answer = minOf(answer, stones[window.first()])
-        }
+
+        if (i >= k - 1) answer = minOf(answer, stones[window.first()])
     }
         return answer
     }
