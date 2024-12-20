113a17c84a7dcb57fd7e2a9528a6663fee7902b7
은명준 <140244595+eunmyeungjun@users.noreply.github.com>
2024-12-20 23:38:09 +0900

    [level 2] Title: 귤 고르기, Time: 71.87 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -9,6 +9,7 @@ class Solution {
                 checkMap.put(it,1)
             }
         }
+        
         val countList = checkMap.values.toList().sorted().reversed()
         var count = 0
         for(i in 0 .. countList.size-1){
